820fa4d0dccf5e2528facb75ee42e4980470c9a9
paul-rooney <paulrooney90@gmail.com>
2023-07-03 21:06:54 +0100

    Fixed shopping list problem
    Shopping list now correctly calculates the total for each ingredient across all meals, even duplicates

--- src/components/Logbook/index.jsx
+++ src/components/Logbook/index.jsx
@@ -19,7 +19,12 @@ function getRandomCombinations(objects, range1, range2, n) {
         const newKcalSum = kcalSum + currentObject.kcal;
         const newProteinSum = proteinSum + currentObject.protein;
 
-        if (newKcalSum >= range1[0] && newKcalSum <= range1[1] && newProteinSum >= range2[0] && newProteinSum <= range2[1]) {
+        if (
+            newKcalSum >= range1[0] &&
+            newKcalSum <= range1[1] &&
+            newProteinSum >= range2[0] &&
+            newProteinSum <= range2[1]
+        ) {
             const combination = [...arr, currentObject];
             const combinationString = JSON.stringify(combination);
 
@@ -116,19 +121,26 @@ const Logbook = ({ recipes }) => {
     const generateShoppingList = () => {
         let arr = mealPlan.flat().map((item) => item.id);
 
-        readRows("recipes_ingredients", `id, quantity, unit, ingredients (id, display_name)`, arr).then((ingredients) => {
-            let x = ingredients.map((item) => ({
-                id: item.id,
-                ingredient_id: item.ingredients.id,
-                display_name: item.ingredients.display_name,
-                quantity: item.quantity,
-                unit: item.unit,
-            }));
-            let y = Object.entries(groupBy(x, "display_name"));
-            // the ReadRows function is getting all of the recipes_ingredients for each recipe in the mealPlan, but this does not account for duplicate meals at the minute
-            setShoppingList(y);
-            // console.log(x.map(item => item.display_name).join(","));
-        });
+        readRows("recipes_ingredients", `id, recipe_id, quantity, unit, ingredients (id, display_name)`, arr).then(
+            (ingredients) => {
+                const groupedMealsWithIngredients = mealPlan
+                    .flat()
+                    .map((meal) =>
+                        ingredients
+                            .filter((item) => meal.id === item.recipe_id)
+                            .map((item) => ({
+                                ...meal,
+                                ingredient_id: item.ingredients.id,
+                                ingredient_display_name: item.ingredients.display_name,
+                                quantity: item.quantity,
+                                unit: item.unit,
+                            }))
+                    )
+                    .flat();
+
+                setShoppingList(Object.entries(groupBy(groupedMealsWithIngredients, "ingredient_display_name")));
+            }
+        );
     };
 
     return (
@@ -145,26 +157,58 @@ const Logbook = ({ recipes }) => {
                         <Switcher threshold="280px" space="var(--size-1)" limit="2">
                             <Stack space="var(--size-1)">
                                 <label className={styles.label}>Minimum kcal</label>
-                                <input id="minKcal" className={styles.input} type="number" step={10} defaultValue={1400}></input>
+                                <input
+                                    id="minKcal"
+                                    className={styles.input}
+                                    type="number"
+                                    step={10}
+                                    defaultValue={1400}
+                                ></input>
                             </Stack>
                             <Stack space="var(--size-1)">
                                 <label className={styles.label}>Maximum kcal</label>
-                                <input id="maxKcal" className={styles.input} type="number" step={10} defaultValue={1550}></input>
+                                <input
+                                    id="maxKcal"
+                                    className={styles.input}
+                                    type="number"
+                                    step={10}
+                                    defaultValue={1550}
+                                ></input>
                             </Stack>
                         </Switcher>
                         <Switcher threshold="280px" space="var(--size-1)" limit="3">
                             <Stack space="var(--size-1)">
                                 <label className={styles.label}>Minimum protein</label>
-                                <input id="minProtein" className={styles.input} type="number" step={1} defaultValue={120}></input>
+                                <input
+                                    id="minProtein"
+                                    className={styles.input}
+                                    type="number"
+                                    step={1}
+                                    defaultValue={120}
+                                ></input>
                             </Stack>
                             <Stack space="var(--size-1)">
                                 <label className={styles.label}>Maximum protein</label>
-                                <input id="maxProtein" className={styles.input} type="number" step={1} defaultValue={160}></input>
+                                <input
+                                    id="maxProtein"
+                                    className={styles.input}
+                                    type="number"
+                                    step={1}
+                                    defaultValue={160}
+                                ></input>
                             </Stack>
                         </Switcher>
                         <Stack space="var(--size-1)">
                             <label className={styles.label}>Number of days</label>
-                            <input id="numDays" className={styles.input} type="number" min={1} max={14} step={1} defaultValue={7}></input>
+                            <input
+                                id="numDays"
+                                className={styles.input}
+                                type="number"
+                                min={1}
+                                max={14}
+                                step={1}
+                                defaultValue={7}
+                            ></input>
                         </Stack>
                         <button type="submit" className={styles.addButton}>
                             <Icon space=".5ch" direction="ltr" icon="plus">
@@ -180,14 +224,19 @@ const Logbook = ({ recipes }) => {
                                   <Stack key={index} space="var(--size-2)">
                                       <Cluster justify="space-between" align="baseline">
                                           <h3>Day {index + 1}</h3>
-                                          <button className={styles.addButton} onClick={() => updateMealPlan(index, [1400, 1550], [120, 160], 1)}>
+                                          <button
+                                              className={styles.addButton}
+                                              onClick={() => updateMealPlan(index, [1400, 1550], [120, 160], 1)}
+                                          >
                                               <Icon space=".5ch" direction="ltr" icon="refresh-cw" />
                                           </button>
                                       </Cluster>
                                       {day.map((meal) => (
                                           <Fragment key={`${meal.id}-${index}`}>
                                               <Stack space="var(--size-1)">
-                                                  <span style={{ color: "var(--jungle-10)", fontWeight: "600" }}>{meal.display_name}</span>
+                                                  <span style={{ color: "var(--jungle-10)", fontWeight: "600" }}>
+                                                      {meal.display_name}
+                                                  </span>
                                                   <Cluster>
                                                       <span>kcal: {meal.kcal}</span>
 
@@ -216,6 +265,7 @@ const Logbook = ({ recipes }) => {
                 <summary>
                     <h3>Shopping list</h3>
                 </summary>
+                <button onClick={generateShoppingList}>Generate shopping list</button>
                 <Grid space="var(--size-2)">
                     {shoppingList.length > 1
                         ? shoppingList
@@ -229,7 +279,7 @@ const Logbook = ({ recipes }) => {
                                   return (
                                       <div style={{ fontSize: "var(--font-size-0)" }} key={value.id}>
                                           <Cluster space="var(--size-3)">
-                                              <span>{value[0].display_name}</span>
+                                              <span>{value[0].ingredient_display_name}</span>
                                               <span key={value.id}>
                                                   Total:{" "}
                                                   {value.reduce((acc, item) => {
@@ -252,15 +302,6 @@ const Logbook = ({ recipes }) => {
                                                       return acc + q;
                                                   }, 0)}
                                               </span>
-                                          </Cluster>
-                                          <Cluster space="var(--size-1)">
-                                              Breakdown:
-                                              {value.map((item) => (
-                                                  <span key={value.id}>
-                                                      {item.quantity}
-                                                      {item.unit}
-                                                  </span>
-                                              ))}
                                           </Cluster>
                                       </div>
                                   );
